ac170b2d52dddec2248a6d05c30b06e17ca7f6e7
GustavoHeimburg <gutogustavogu@gmail.com>
2023-10-15 23:29:25 -0300

    Att

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {
+  BrowserRouter as Router, Routes, Route,
+} from "react-router-dom";
+import Home from "./Componetes/Home";
+import PaginaChat from "./Componetes/PaginaChat";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path='/chatpagina' element={<PaginaChat/>}/>
+              <Route path='/' element={<Home/>}/>
+          </Routes>
+        </div>
+      </Router>
+
+);
 }
 
 export default App;
---
+++ src/Componetes/PaginaChat.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function PaginaChat(){
+    return(
+
+        <div className='chatpage'>
+
+        </div>
+    );
+
+}
+export default PaginaChat
